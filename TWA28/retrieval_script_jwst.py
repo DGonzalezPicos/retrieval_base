b5057cece93a017783272c3cf15f0976e99cecf2
DGonzalezPicos <dariogonzalezpicos@gmail.com>
2024-02-24 10:44:05 +0100

    Add pre-processing and pRT_atm creation to retrieval script

--- TWA28/retrieval_script_jwst.py
+++ TWA28/retrieval_script_jwst.py
@@ -3,12 +3,13 @@ import pathlib
 
 from retrieval_base.retrieval import pre_processing, prior_check, Retrieval
 from retrieval_base.spectrum_jwst import SpectrumJWST
+from retrieval_base.pRT_model import pRT_model
 import retrieval_base.auxiliary_functions as af
 from retrieval_base.parameters import Parameters
 
 import config_jwst as conf
 
-
+conf_data = conf.config_data['G395H_F290LP']
 # create output directory
 
 for key in ['data', 'plots']:
@@ -25,6 +26,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.pre_processing:
+        
+        ## Pre-processing data
         spec = SpectrumJWST(file='jwst/TWA28_g395h-f290lp.fits')
         spec.split_grism(4155., keep=1)
         # spec.sigma_clip(sigma=3, width=5, max_iter=5, fun='median')
@@ -34,6 +37,30 @@ if __name__ == '__main__':
 
         af.pickle_save(f'{conf.prefix}data/d_spec_{spec.w_set}.pkl', spec)
         print(f'--> Saved {f"{conf.prefix}data/d_spec_{spec.w_set}.pkl"}')
+
+
+        ## Create pRT_atm object
+        pRT_file =pathlib.Path(f'{conf.prefix}data/pRT_atm_{spec.w_set}.pkl')
+        if not pRT_file.exists():
+            print(f'--> Creating {pRT_file}')
+
+            pRT_atm = pRT_model(
+                line_species=conf.line_species, 
+                d_spec=spec, 
+                mode='lbl', 
+                lbl_opacity_sampling=conf_data['lbl_opacity_sampling'], 
+                cloud_species=conf.cloud_species, 
+                # rayleigh_species=['H2', 'He'], 
+                # continuum_opacities=['H2-H2', 'H2-He'], 
+                rayleigh_species=conf.rayleigh_species,
+                continuum_opacities=conf.continuum_opacities,
+                log_P_range=conf_data.get('log_P_range'), 
+                n_atm_layers=conf_data.get('n_atm_layers'), 
+                rv_range=conf.free_params['rv'][0], 
+                )
+            # check parent directory
+            # pRT_file.parent.mkdir(parents=True, exist_ok=True)
+            af.pickle_save(pRT_file, pRT_atm)
 
     if args.prior_check:
         print('--> Running prior predictive check..')

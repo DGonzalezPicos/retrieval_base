431b9091860ac79d3ca2f16e3a918ca20d41ca6f
DGonzalezPicos <dariogonzalezpicos@gmail.com>
2024-07-22 16:52:52 +0200

    chore: Update retrieval_script_jwst.py
    - Update run parameter to lbl12_KM_2 in config_jwst.py
    - Add ScH to opacity parameters for retrieval
    - Improve memory management and profiling
    - Implement corr-k retrieval on K-band
    - Fix `lbl_opacity_sampling=35` for speed
    - Disable GP, enable global error scaling, and set lbl to 10
    - Fix GP hyperparameters and PMN accuracy
    - Add opacities, fix bug with wave_cm for `nirspersions`, and fix bug with GP `log_l` prior and max. separation

--- TWA27A/config_jwst.py
+++ TWA27A/config_jwst.py
@@ -7,21 +7,26 @@ file_params = 'config_jwst.py'
 # Files and physical parameters
 ####################################################################################
 
-prefix = 'jwst_1'
-prefix = f'./retrieval_outputs/{prefix}/test_'
+# run = 'ck_K_2'
+run = 'lbl12_KM_1'
+prefix = f'./retrieval_outputs/{run}/test_'
 
 config_data = {
-    'G395H_F290LP': {
-        'w_set': 'G395H_F290LP', 'wave_range': (4100, 5300), 
-        # 'w_set': 'K2166', 'wave_range': (1900, 2500), 
-
-        'file_target': './jwst/TWA28_g395h-f290lp.fits', 
+    'NIRSpec': {
+        # 'w_set': 'G395H_F290LP', 'wave_range': (4100, 5300), 
+        'w_set': 'NIRSpec',
+        # 'wave_range': (1650, 3200), # g235h-f170lp
+        'wave_range': (1650, 5300), 
+        # 'wave_range': (1630, 3250), 
         
-        'lbl_opacity_sampling' : 20,
+        'lbl_opacity_sampling' : 12,
+        # 'lbl_opacity_sampling' : None,
         'sigma_clip': 3,
-        'sigma_clip_width': 50, 
+        'sigma_clip_width': 21, 
+        'Nedge': 80,
     
-        'log_P_range': (-6,2), 'n_atm_layers': 30, 
+        'log_P_range': (-5,2),
+        'n_atm_layers': 35, 
         }, 
     }
 
@@ -29,69 +34,144 @@ config_data = {
 ####################################################################################
 # Model parameters
 ####################################################################################
+opacity_params = {
+    'log_12CO': ([(-14,-2), r'$\log\ \mathrm{^{12}CO}$'], 'CO_high'),
+    'log_13CO': ([(-14,-2), r'$\log\ \mathrm{^{13}CO}$'], 'CO_36_high'),
+    'log_C18O': ([(-14,-2), r'$\log\ \mathrm{C^{18}O}$'], 'CO_28'),
+    'log_C17O': ([(-14,-2), r'$\log\ \mathrm{C^{17}O}$'], 'CO_27'),
+    
+    'log_H2O': ([(-14,-2), r'$\log\ \mathrm{H_2O}$'], 'H2O_pokazatel_main_iso'),
+    'log_H2O_181': ([(-14,-2), r'$\log\ \mathrm{H_2^{18}O}$'], 'H2O_181_HotWat78'),
+    # 'log_HDO': ([(-14,-2), r'$\log\ \mathrm{HDO}$'], 'HDO_voronin'),
+    'log_HF': ([(-14,-2), r'$\log\ \mathrm{HF}$'], 'HF_main_iso'), # DGP (2024-07-16): accidentally removed 
+    'log_HCl': ([(-14,-2), r'$\log\ \mathrm{HCl}$'], 'HCl_main_iso'), # DGP (2024-07-16): try this one
+    
+    'log_CO2': ([(-14,-2), r'$\log\ \mathrm{CO_2}$'], 'CO2_main_iso'),
+    'log_CN': ([(-14,-2), r'$\log\ \mathrm{CN}$'], 'CN_high'),
+    
+    'log_Na': ([(-14,-2), r'$\log\ \mathrm{Na}$'], 'Na_allard'),
+    'log_K': ([(-14,-2), r'$\log\ \mathrm{K}$'], 'K'),
+    'log_Ca': ([(-14,-2), r'$\log\ \mathrm{Ca}$'], 'Ca'),
+    'log_Ti': ([(-14,-2), r'$\log\ \mathrm{Ti}$'], 'Ti'),
+    # 'log_Mg': ([(-14,-2), r'$\log\ \mathrm{Mg}$'], 'Mg'),
+    'log_Mn': ([(-14,-2), r'$\log\ \mathrm{Mn}$'], 'Mn'),
+    # 'log_Fe': ([(-14,-2), r'$\log\ \mathrm{Fe}$'], 'Fe'),
+    'log_Al': ([(-14,-2), r'$\log\ \mathrm{Al}$'], 'Al'),
+    
+    'log_FeH': ([(-14,-2), r'$\log\ \mathrm{FeH}$'], 'FeH_main_iso'),
+    # 'log_CrH': ([(-14,-2), r'$\log\ \mathrm{CrH}$'], 'CrH_main_iso'),
+    # 'log_TiH': ([(-14,-2), r'$\log\ \mathrm{TiH}$'], 'TiH_main_iso'),
+    'log_CaH': ([(-14,-2), r'$\log\ \mathrm{CaH}$'], 'CaH_XAB_main_iso'),
+    'log_AlH': ([(-14,-2), r'$\log\ \mathrm{AlH}$'], 'AlH_main_iso'),
+    'log_MgH': ([(-14,-2), r'$\log\ \mathrm{MgH}$'], 'MgH_main_iso'),
+    'log_NaH': ([(-14,-2), r'$\log\ \mathrm{NaH}$'], 'NaH_main_iso'), # DGP (2024-07-16)
+    # 'log_ScH': ([(-14,-2), r'$\log\ \mathrm{ScH}$'], 'ScH_main_iso'), # DGP (2024-07-16): try
 
+    'log_OH': ([(-14,-2), r'$\log\ \mathrm{OH}$'], 'OH_MoLLIST_main_iso'),
+    # 'log_H2': ([(-12,-0.1), r'$\log\ \mathrm{H_2}$'], 'H2_main_iso'),
+    
+    'log_VO': ([(-14,-2), r'$\log\ \mathrm{VO}$'], 'VO_HyVO_main_iso'), # DGP (2024-07-16): 3.4 um bump?
+    'log_TiO': ([(-14,-2), r'$\log\ \mathrm{TiO}$'], 'TiO_48_Exomol_McKemmish'),
+    # 'log_SiO': ([(-14,-2), r'$\log\ \mathrm{SiO}$'], 'SiO_SiOUVenIR_main_iso'),
+    # 'log_AlO': ([(-14,-2), r'$\log\ \mathrm{AlO}$'], 'AlO_main_iso'),
+    # 'log_H2S': ([(-14,-2), r'$\log\ \mathrm{H_2S}$'], 'H2S_Sid_main_iso'),
+}
+print(f' --> {len(opacity_params)} opacity parameters')
 # Define the priors of the parameters
 free_params = {
 
     # Uncertainty scaling
-    'log_a': [(-2,0.6), r'$\log\ a$'], 
-    'log_l': [(-1,0.2), r'$\log\ l$'], 
+    # 'log_a_G': [(-2,0.6), r'$\log\ a$'], 
+    # 'log_a_G235': [(-2,0.6), r'$\log\ a_{G235}$'],
+    # 'log_a_G395': [(-2,0.6), r'$\log\ a_{G395}$'],
+    # 'log_l': [(-2,0.3), r'$\log\ l$'], 
+    'beta_G' : [(1., 20.), r'$\beta$'], # (NEW 2024-06-11): manage underestimated errors without inflating the GP kernel
 
     # General properties
     # R = 0.29 [R_sun]
     # convert to jupiter radii
     # R = 0.29 * 9.73116 = 2.82 [R_jup]
-    # 'R_p': [(1.0, 10.0), r'$R_\mathrm{p}$'], 
-    'log_g': [(2.5,5.0), r'$\log\ g$'], 
+    # 'R_p': [(1.0, 5.0), r'$R_\mathrm{p}$'], # use this for robust results
+     'R_p': [(1.5, 4.0), r'$R_\mathrm{p}$'], # R_p ~ 2.82 R_jup
+    # 'R_p': [(2.72, 2.72), r'$R_\mathrm{p}$'], # R_p ~ 2.82 R_jup
+    'log_g': [(2.0,5.0), r'$\log\ g$'], 
     # 'epsilon_limb': [(0.1,0.98), r'$\epsilon_\mathrm{limb}$'], 
-
+    
+    # veiling parameters
+    # 'log_r_0': [(-20, -14), r'$\log\ r_0$'], # veiling amplitude at wave=min(wave)
+    # 'alpha': [(1.0, 20.0), r'$\alpha$'], # veiling power-law index, should be positive for dust emission
+    'R_d': [(1.0, 30.0), r'$R_d [R_{Jup}]$'], # disk radius in R_jup
+    # 'R_d': [(14.0, 15.0), r'$R_d [R_{Jup}]$'], # disk radius in R_jup
+    # 'log_R_d' : [(-2, 4), r'$\log\ R_d$'], # disk radius in R_jup
+    'T_d': [(100, 800), r'$T_d$'], # disk temperature in K
+    # 'T_d': [(100, 101), r'$T_d$'], # disk temperature in K
     # Velocities
     # 'vsini': [(2,30), r'$v\ \sin\ i$'], 
-    'rv': [(-40,40), r'$v_\mathrm{rad}$'], 
-    
-    # Chemistry
-    'log_12CO': [(-12,-2), r'$\log\ \mathrm{^{12}CO}$'], 
-    'log_13CO': [(-10,-2), r'$\log\ \mathrm{^{13}CO}$'], 
-    'log_C18O': [(-12,-2), r'$\log\ \mathrm{C^{18}O}$'], 
-    'log_C17O': [(-12,-2), r'$\log\ \mathrm{C^{17}O}$'],
-    
-    'log_H2O': [(-12,-2), r'$\log\ \mathrm{H_2O}$'], 
-    # 'log_H2O_181': [(-12,-2), r'$\log\ \mathrm{H_2^{18}O}$'],
-    'log_CO2': [(-12,-2), r'$\log\ \mathrm{CO_2}$'],
+    'rv': [(-40.,40.), r'$v_\mathrm{rad}$'],
+    # 'log_H-' : [(-12,-6), r'$\log\ \mathrm{H^-}$'],
 
-    # PT profile
-    'dlnT_dlnP_0': [(0.01, 0.40), r'$\nabla_{T,0}$'], 
-    'dlnT_dlnP_1': [(0.01,0.40), r'$\nabla_{T,1}$'], 
-    'dlnT_dlnP_2': [(0.01,0.40), r'$\nabla_{T,2}$'], 
-    # 'dlnT_dlnP_3': [(0.00,0.20), r'$\nabla_{T,3}$'], 
-    # 'dlnT_dlnP_4': [(-0.05,0.15), r'$\nabla_{T,4}$'], 
-    'dlnT_dlnP_3': [(-0.05,0.40), r'$\nabla_{T,3}$'], 
-    'dlnT_dlnP_4': [(-0.05,0.40), r'$\nabla_{T,4}$'],
-    'T_0': [(3000,10000), r'$T_0$'], 
+   'T_0': [(2000,8000), r'$T_0$'], 
+    'log_P_RCE': [(-3,1), r'$\log\ P_\mathrm{RCE}$'],
+    # 'dlog_P' : [(0.2, 1.6), r'$\Delta\log\ P$'],
+    'dlog_P_1' : [(0.2, 1.6), r'$\Delta\log\ P_1$'], 
+    'dlog_P_3' : [(0.2, 1.6), r'$\Delta\log\ P_3$'],
+    'dlnT_dlnP_RCE': [(0.04, 0.34), r'$\nabla_{T,RCE}$'],
+    'dlnT_dlnP_0':   [(0.04, 0.34), r'$\nabla_{T,0}$'],
+    'dlnT_dlnP_1':   [(0.04, 0.34), r'$\nabla_{T,1}$'],
+    'dlnT_dlnP_2':   [(0.04, 0.34), r'$\nabla_{T,2}$'],
+    'dlnT_dlnP_3':   [(0.00, 0.34), r'$\nabla_{T,3}$'],
+    'dlnT_dlnP_4':   [(0.00, 0.34), r'$\nabla_{T,4}$'],
+    'dlnT_dlnP_5':   [(0.00, 0.34), r'$\nabla_{T,5}$'], # new points
+
     # 'f_slope': [(-0.1, 0.1), r'$f_\mathrm{slope}$'],
-    'res': [(1500, 5000), r'$\mathrm{R}$'], # instrumental spectral resolution
+    # 'res_G235': [(1500, 4000), r'$\mathrm{R}_{G235}$'], # instrumental spectral resolution
+    # 'res_G395': [(1500, 5000), r'$\mathrm{R}_{G395}$'], # instrumental spectral resolution
+    # 'res_M': [(1500, 5000), r'$\mathrm{R}_M$'], # instrumental spectral resolution    
 }
+free_params.update({k:v[0] for k,v in opacity_params.items()})
+
 # Constants to use if prior is not given
-parallax_mas = 15.46 # +- 0.12 mas (Manjavacas+ 2024, Gaia 2020)
+# distance in pc to parallax
+parallax_mas = 16.46 # Gaia DR3
+d_pc = 1e3 / parallax_mas # ~ 59.17 pc
+
+# dlnT_dlnP = [free_params[key] for key in free_params.keys() if 'dlnT_dlnP' in key]
+# log_P_knots = [-5, -3, -2.0, -1.25, -0.5, 0.25, 1.0, 2.0] # 8 knots
+# N_PT_knots = len(log_P_knots) # PT knots = 8 (NEW 2024-05-08)
+# assert len(dlnT_dlnP) == N_PT_knots, 'Number of knots does not match number of dlnT_dlnP parameters'
+PT_interp_mode = 'linear'
+PT_mode = 'RCE'
+
+N_knots = 1 # spline knots (continuum fitting)
 
 constant_params = {
     # General properties
-    'R_p' : 2.8, 
+    # 'R_p' : 1.0, 
     'parallax': parallax_mas, 
-    'epsilon_limb': 0.65, 
-    # 'log_g': 4.0,
+    'epsilon_limb': 0.5, 
+    # 'log_g': 3.5,
     'vsini':1.,
 
     # PT profile
-    'log_P_knots': [-6., -3., -1., 1., 2.], 
-    'N_knots': 5,
+    # 'log_P_knots': [-6., -3., -1., 1., 2.], 
+    # 'log_P_knots': log_P_knots,
+    'N_knots': N_knots, # avoid using spline to fit the continuum
+    # 'res_G235': 2800, # instrumental spectral resolution
+    # 'res_G395': 3000, # instrumental spectral resolution
+    # 'fit_radius': True,
+    'gratings':[
+                'g235h', 
+                # 'g235h',
+                'g395h',
+                # 'g395h'
+                ], 
 }
 
 ####################################################################################
 #
 ####################################################################################
-# N_knots = 5
-scale_flux = True
+scale_flux = False
+scale_flux_eps = 0.00 # no scaling, set to 0.05 for a 5% deviation even with scale_flux=False
 scale_err  = True
 apply_high_pass_filter = False
 
@@ -113,39 +193,23 @@ chem_kwargs = dict()
 # Rayleigh scattering and continuum opacities
 rayleigh_species=['H2','He']
 continuum_opacities=['H2-H2', 'H2-He', 'H-']
+line_species =[v[1] for _,v in opacity_params.items()]
+# add H2 as line species, not a free parameter
+# abundance of H2 calculated to sum(VMR) = 1
+# line_species.append('H2_main_iso') # TODO: this?
 
-line_species = [
-    'CO_high', 
-    'CO_36_high', 
-    'CO_28', 
-    'CO_27', 
-
-    'H2O_pokazatel_main_iso', 
-    # 'H2O_181',
-    'CO2_main_iso',
-    ]
-species_to_plot_VMR = [
-    '12CO', '13CO', 'H2O',
-    'CO2'
-    ]
-species_to_plot_CCF = [
-    '12CO', '13CO', 'H2O',
-    'CO2',
-    'H2O_181', 
-    'C18O', 
-    'C17O'
-    ]
+species_to_plot_VMR , species_to_plot_CCF = [], []
 
 ####################################################################################
 # Covariance parameters
 ####################################################################################
 
-cov_mode = 'GP'
+cov_mode = None
 
 cov_kwargs = dict(
-    trunc_dist   = 3, 
+    trunc_dist   = 1, # set to 3 for accuracy, 2 for speed
     scale_GP_amp = True, 
-    max_separation = 20, 
+    # max_separation = 20, 
 
     # Prepare the wavelength separation and
     # average squared error arrays and keep 
@@ -155,20 +219,22 @@ cov_kwargs = dict(
 if free_params.get('log_l') is not None:
     cov_kwargs['max_separation'] =  cov_kwargs['trunc_dist']
     cov_kwargs['max_separation'] *= 10**free_params['log_l'][0][1]
+    
 ####################################################################################
 # PT parameters
 ####################################################################################
 
-PT_mode = 'free_gradient'
 
 PT_kwargs = dict(
     conv_adiabat = True, 
 
     ln_L_penalty_order = 3, 
-    PT_interp_mode = 'quadratic', 
+    PT_interp_mode = PT_interp_mode, 
 
     enforce_PT_corr = False, 
-    n_T_knots = 5, 
+    # n_T_knots = N_PT_knots,
+    sonora=dict(teff=2400, log_g=4.0),
+    
 )
 
 ####################################################################################
@@ -176,39 +242,16 @@ PT_kwargs = dict(
 ####################################################################################
 
 const_efficiency_mode = True
-sampling_efficiency = 0.05
-evidence_tolerance = 0.5
+sampling_efficiency = 0.10
+evidence_tolerance = 1.0
 n_live_points = 100
-n_iter_before_update = 1
-
+n_iter_before_update = n_live_points * 2
+# n_iter_before_update = 1
 # generate a .txt version of this file
 
 if __name__ == '__main__':
-    # print all global variables
-    save_attrs = ['config_data', 'magnitudes', 'free_params',
-                'd_pc', 'parallax', 'parallax_mas',
-                'constant_params', 'scale_flux', 'scale_err',
-                'apply_high_pass_filter', 'cloud_mode', 'cloud_species', 
-                'mask_lines', 'chem_mode', 'chem_kwargs', 'rayleigh_species', 
-                'continuum_opacities', 'line_species',
-                'cov_mode', 'cov_kwargs', 'PT_mode', 'PT_kwargs',
-                'const_efficiency_mode', 'sampling_efficiency',
-                'evidence_tolerance', 'n_live_points', 'n_iter_before_update']
-
+    from retrieval_base.config import Config
     import pathlib
-    import json
-
-    # get path of this file
-    path = pathlib.Path(__file__).parent.absolute()
-    outpath = path / f'{prefix[2:]}data'
-    outfile = outpath / file_params.replace('.py', '.txt')
-    outfile.parent.mkdir(parents=True, exist_ok=True)
-
-    with open(outfile, 'w') as file:
-        file.write(json.dumps({key: globals()[key] for key in save_attrs}))
     
-    file.close()
-    print(f'Wrote {outfile}')
-    # # # test loading the file with json
-    # with open(outfile, 'r') as file:
-    #     load_file = json.load(file)
+    conf = Config(path=pathlib.Path(__file__).parent.absolute(), target=None, run=run)
+    conf.save_json(file_params, globals())
--- TWA27A/retrieval_script_jwst.py
+++ TWA27A/retrieval_script_jwst.py
@@ -1,16 +1,22 @@
 import argparse
 import pathlib
+import subprocess as sp
+import numpy as np
 
 from retrieval_base.retrieval import pre_processing, prior_check, Retrieval
 from retrieval_base.spectrum_jwst import SpectrumJWST
 from retrieval_base.pRT_model import pRT_model
 import retrieval_base.auxiliary_functions as af
 from retrieval_base.parameters import Parameters
-
+from retrieval_base.config import Config
 import config_jwst as conf
 
+config_file = 'config_jwst.txt'
 target = 'TWA27A'
-conf_data = conf.config_data['G395H_F290LP']
+run = None
+# run = 'jwst_KLM_N10_veiling3'
+
+conf_data = conf.config_data['NIRSpec']
 # create output directory
 
 for key in ['data', 'plots']:
@@ -24,7 +30,10 @@ if 'dgonzalezpi' in cwd:
     import matplotlib
     # disable interactive plotting
     matplotlib.use('Agg')
-    # path = pathlib.Path('/home/dgonzalezpi/retfish/')
+    path = pathlib.Path('/home/dgonzalezpi/retrieval_base/')
+else:
+    path = pathlib.Path('/home/dario/phd/retrieval_base')
+    
 
 # Instantiate the parser
 parser = argparse.ArgumentParser()
@@ -32,17 +41,50 @@ parser.add_argument('--pre_processing', '-p', action='store_true', default=False
 parser.add_argument('--prior_check', '-c', action='store_true', default=False)
 parser.add_argument('--retrieval', '-r', action='store_true', default=False)
 parser.add_argument('--evaluation', '-e', action='store_true', default=False)
+# parser.add_argument('--time_profiler', '-t', action='store_true', default=False)
+parser.add_argument('--memory_profiler', '-m', action='store_true', default=False)
 args = parser.parse_args()
 
 if args.pre_processing:
-    
+    sp.call(['python', f'{path}/{target}/config_jwst.py'])
     ## Pre-processing data
-    spec = SpectrumJWST(file=f'jwst/{target}_g395h-f290lp.fits')
-    spec.split_grating(4155., keep=1)
-    # spec.sigma_clip(sigma=3, width=5, max_iter=5, fun='median')
-    spec.sigma_clip(spec.err, sigma=3, width=50, max_iter=5, fun='median')
-    spec.reshape(1,1)
-    spec.prepare_for_covariance()
+    
+    # gratings = [
+    #         # 'g140h-f100lp', 
+    #         'g235h-f170lp', 
+    #         'g395h-f290lp',
+    #         ]
+    gratings_dict = {'g140h': 'g140h-f100lp',
+                     'g235h': 'g235h-f170lp', 
+                     'g395h': 'g395h-f290lp'}
+    
+    gratings_list = list(set(conf.constant_params['gratings']))
+    gratings = [gratings_dict[g] for g in gratings_list]
+    
+    # each grating has two filters, make list [a,b] to [a,a,b,b]
+    # gratings_list = [g.split('-')[0] for g in gratings for _ in range(2)]
+    # print(f'--> Loading data for {gratings_list}')
+    
+    files = [f'jwst/{target}_{g}.fits' for g in gratings]
+    Nedge = conf_data.get('Nedge', 40)
+    spec = SpectrumJWST(Nedge=Nedge).load_gratings(files)
+    spec.reshape(spec.n_orders, 1)
+    # spec.fix_wave_nans() # experimental...
+    spec.sigma_clip_reshaped(use_flux=False, 
+                                # sigma=3, # KM bands
+                                sigma=conf_data.get('sigma', 2),
+                                width=conf_data.get('sigma_clip_width', 30),
+                                max_iter=5,
+                                fun='median', 
+                                debug=False)
+    # spec.scatter_overlapping_points()
+    # spec.apply_error_scaling()
+    spec.plot_orders(fig_name=f'{conf.prefix}plots/spec_to_fit.pdf', grid=True)
+    
+    if conf.cov_mode == 'GP':
+        spec.prepare_for_covariance()
+        
+    spec.gratings_list = conf.constant_params['gratings']
 
     af.pickle_save(f'{conf.prefix}data/d_spec_{spec.w_set}.pkl', spec)
     print(f'--> Saved {f"{conf.prefix}data/d_spec_{spec.w_set}.pkl"}')
@@ -52,12 +94,12 @@ if args.pre_processing:
     pRT_file =pathlib.Path(f'{conf.prefix}data/pRT_atm_{spec.w_set}.pkl')
     if not pRT_file.exists():
         print(f'--> Creating {pRT_file}')
-
+        lbl = conf_data['lbl_opacity_sampling']
         pRT_atm = pRT_model(
             line_species=conf.line_species, 
             d_spec=spec, 
-            mode='lbl', 
-            lbl_opacity_sampling=conf_data['lbl_opacity_sampling'], 
+            mode='lbl' if (lbl is not None) else 'c-k',
+            lbl_opacity_sampling=lbl,
             cloud_species=conf.cloud_species, 
             # rayleigh_species=['H2', 'He'], 
             # continuum_opacities=['H2-H2', 'H2-He'], 
@@ -70,26 +112,46 @@ if args.pre_processing:
         # check parent directory
         # pRT_file.parent.mkdir(parents=True, exist_ok=True)
         af.pickle_save(pRT_file, pRT_atm)
+        print(f'   --> Saved {pRT_file}')
 
 if args.prior_check:
     print('--> Running prior predictive check..')
     figs_path = pathlib.Path(f'{conf.prefix}plots/')
     figs_path.mkdir(parents=True, exist_ok=True)
     
-    prior_check(conf=conf, fig_name=figs_path / 'prior_predictive_check.pdf')
-
+    random = False
+    random_label = '_random' if random else ''
+    ret = prior_check(conf=conf, n=3, 
+                random=random, 
+                get_contr=False,
+                fig_name=figs_path / f'prior_predictive_check{random_label}.pdf')
+    
+    if args.memory_profiler:
+        print('--> Running memory profiler..')
+        ret.list_memory_allocation(min_size_mb=0.1)
+        # print(ret.w_set)
+        for w_set in ret.d_spec.keys():
+            ret.list_memory_allocation(obj=ret.pRT_atm[w_set], min_size_mb=0.1)
+        
 if args.retrieval:
     ret = Retrieval(
         conf=conf, 
-        evaluation=args.evaluation
+        evaluation=args.evaluation,
+        # tmp_path=args.tmp_path
         )
     ret.PMN_run()
 
 if args.evaluation:
+    if run is not None: # override config file
+        conf = Config(path=path, target=target, run=run)
+        conf(config_file)        
+    
     ret = Retrieval(
         conf=conf, 
         evaluation=args.evaluation
         )
+    ret.plot_ccf = False
+    
     ret.PMN_callback_func(
         n_samples=None, 
         n_live=None, 

Fix C17O VMR profile calculation and add missing opacity profiles

retrieval_base/chemistry.py
@@ -114,7 +114,7 @@ class Chemistry:
         
         #'H2': ('C16', r'H$_2$'), 
         'HD': ('C17', r'HD'), 
-        'OH': ('C18', r'OH'),
+        'OH': ('#FF7B0D', r'OH'), # orange
 
         'K': ('C18', r'K'), 
         # 'Na': ('purple', r'Na'), 
@@ -307,18 +307,19 @@ class FreeChemistry(Chemistry):
                     # Single value given: constant, vertical profile
                     VMR_i = self.VMRs[species_i] * np.ones(self.n_atm_layers)
                     
-                    if species_i == '13CO' and self.VMRs.get(f'12CO_1') is not None:
-                        # 12CO is given with a altitude profile, normalized to middle layer
-                        VMR_i *= (self.VMRs['12CO'] / self.VMRs['12CO_1'])
-                    if species_i == 'C18O' and self.VMRs.get(f'12CO_1') is not None:
-                        # 12CO is given with a altitude profile
-                        VMR_i *= (self.VMRs['12CO'] / self.VMRs['12CO_1'])
-                    if species_i == 'C17O' and self.VMRs.get(f'12CO_1') is not None:
-                        VMR_i *= (self.VMRs['12CO'] / self.VMRs['12CO_1'])
+                    iso_profiles = {'13CO': '12CO',
+                                    'C18O': '12CO',
+                                    'C17O': '12CO',
+                                    'H2O_181': 'H2O'}
+                    # iso_profiles = {}
+                    if len(iso_profiles) > 0:
+                        for k, v in iso_profiles.items():
+                            if species_i == k and self.VMRs.get(v) is not None:
                     
-                    if species_i == 'H2O_181' and self.VMRs.get(f'H2O_1') is not None:
-                        # H2O is given with a altitude profile
-                        VMR_i *= (self.VMRs['H2O'] / self.VMRs['H2O_1'])                          
+                                VMR_i = (self.VMRs[v] / self.VMRs[v].max()) * self.VMRs[k]
+                                # check that is is smaller than 1.0
+                                assert (VMR_i <= 1.0).all(), f'VMR_{v} = {VMR_i} > 1.0'         
+                       
 
                 if self.VMRs.get(f'{species_i}_0') is not None:
                     # Multiple values given, use spline interpolation
@@ -364,7 +365,8 @@ class FreeChemistry(Chemistry):
         #     self.mass_fractions['H2'] = self.read_species_info('H2', 'mass') * 
             
         self.mass_fractions['H2'] = self.read_species_info('H2', 'mass') * (1 - VMR_wo_H2)
-        
+        # assert self.mass_fractions['H2'].all() < 1, 'H2 mass fraction > 1'
+        # assert self.mass_fractions['He'].all() < 1, 'He mass fraction > 1'
     
         # self.mass_fractions['H-'] = 6e-9 # solar
         self.mass_fractions['H-'] = self.VMRs.get('H-', 6e-9)
@@ -376,9 +378,10 @@ class FreeChemistry(Chemistry):
         H += self.read_species_info('H2', 'H') * (1 - VMR_wo_H2)
         self.mass_fractions['H'] = H
 
-        if VMR_wo_H2.any() > 1:
+        if VMR_wo_H2.any() > 1: #or (self.mass_fractions['H2'] > 1).any():
             # Other species are too abundant
             self.mass_fractions = -np.inf
+            print(f' VMR_wo_H2 = {VMR_wo_H2} > 1 --> mass_fractions = -np.inf')
             return self.mass_fractions
 
         # Compute the mean molecular weight from all species

retrieval_base/figures.py
@@ -840,7 +840,8 @@ def fig_VMR(Chem,
             ylim=(np.max(pressure), np.min(pressure)),
             )
     if showlegend:
-        ax.legend()
+        ncol = len(species_to_plot)//2
+        ax.legend(ncol=ncol, loc=(0.00, 1.01+0.08*(ncol-3)), frameon=False)
     if (fig_name is not None):
         fig.savefig(fig_name)
         print(f'--> Saved {fig_name}')

retrieval_base/retrieval.py
@@ -377,9 +377,9 @@ def prior_check(conf, n=3,
     # first evaluation the model at 'n' different parameter values
     if random:
         # random values between 0 and 1
-        theta = np.random.rand(n)
+        theta = [np.random.rand(len(ret.Param.param_keys)) for _ in range(n)]
     else:
-        theta = np.linspace(0, 1, n)
+        theta = np.linspace(0, 1, n) * np.ones(len(ret.Param.param_keys))
     m_spec_list = []
     logL_list = [] 
     # plot PT
@@ -388,14 +388,19 @@ def prior_check(conf, n=3,
     fig_chem, ax_chem = plt.subplots(1,1, figsize=(5,5))
     lss = ['-', '--', ':' , '-.', '-', '--', ':' , '-.']
     time_list = []
-    for i, theta_i in enumerate(theta):
+    for i in range(n):
         start = time.time()
 
-        ret.Param(theta_i * np.ones(len(ret.Param.param_keys)))
+        # ret.Param(theta_i * np.ones(len(ret.Param.param_keys)))
+        theta_i = theta[i]
+        ret.Param(theta_i)
         sample = {k:ret.Param.params[k] for k in ret.Param.param_keys}
         print(sample)
         ret.evaluation = get_contr
         ln_L = ret.PMN_lnL_func()
+        if ln_L == -np.inf:
+            print(f'ln_L = -inf\n')
+            continue
         # assert hasattr(ret.m_spec, 'int_contr_em'), f' No integrated contribution emission found in ret.m_spec'
         print(f'ln_L = {ln_L:.4e}\n')
         end = time.time()
@@ -429,6 +434,7 @@ def prior_check(conf, n=3,
                         pressure=ret.PT.pressure,
                         showlegend=(i==0),
                         ls=lss[i],
+                        xlim=[1e-12, 1e0],
                         fig_name=str(fig_name).replace('.pdf', f'_VMR.pdf') if i==(len(theta)-1) else None)
 
     
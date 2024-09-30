b802cdd3b52f001e5d3a9101cbb14849276440d2
DGonzalezPicos <dariogonzalezpicos@gmail.com>
2024-09-30 13:26:15 +0200

    Refactor retrieval_script.py to update run variable and add resume option

---
+++ gl725B/retrieval_script.py
@@ -0,0 +1,85 @@
+import argparse
+import subprocess
+    
+from retrieval_base.retrieval import pre_processing_spirou, prior_check, Retrieval
+from retrieval_base.config import Config
+from retrieval_base.auxiliary_functions import get_path
+path = get_path()
+
+import config_freechem as conf
+
+
+
+config_file = 'config_freechem.txt'
+target = 'gl725B'
+run = 'sphinx1' # important to set this to the correct run
+
+if __name__ == '__main__':
+
+    # Instantiate the parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pre_processing', '-p', action='store_true')
+    parser.add_argument('--check', '-c', action='store_true')
+    parser.add_argument('--retrieval', '-r', action='store_true')
+    parser.add_argument('--evaluation', '-e', action='store_true')
+    parser.add_argument('--ccf', '-ccf', action='store_true', help='Cross-correlation function', default=False)
+    parser.add_argument('--resume', '-res', action='store_true', default=False)
+
+    # parser.add_argument('--synthetic', action='store_true')
+    args = parser.parse_args()
+
+    if args.pre_processing:
+        assert conf.run == run, f'Run {run} does not match run in config file {conf.run}'
+        subprocess.run(['python', 'config_freechem.py'])
+        
+        # for conf_data_i in conf.config_data.values():
+        pre_processing_spirou(conf=conf, conf_data=conf.config_data['spirou'])            
+                        
+    if args.check:
+        # ret = Retrieval(
+        #     conf=conf, 
+        #     evaluation=args.evaluation
+        #     )
+        # ret.prior_check()
+        prior_check(conf,
+                    n=3,
+                    random=True,
+                    w_set='spirou',
+                    fig_name=conf.prefix + 'plots/prior_check.pdf',
+        )
+
+    if args.retrieval:
+        conf = Config(path=path, target=target, run=run)
+        conf(config_file)
+    
+        ret = Retrieval(
+            conf=conf, 
+            evaluation=args.evaluation
+            )
+        ret.PMN_resume = args.resume
+
+        ret.PMN_run()
+
+    if args.evaluation:
+        conf = Config(path=path, target=target, run=run)
+        conf(config_file)
+    
+        ret = Retrieval(
+            conf=conf, 
+            evaluation=args.evaluation,
+            # plot_ccf=args.ccf
+            )
+            
+        ret.PMN_callback_func(
+            n_samples=None, 
+            n_live=None, 
+            n_params=None, 
+            live_points=None, 
+            posterior=None, 
+            stats=None,
+            max_ln_L=None, 
+            ln_Z=None, 
+            ln_Z_err=None, 
+            nullcontext=None
+            )
+
--- paper/carbon_isotope.py
+++ paper/carbon_isotope.py
@@ -59,27 +59,51 @@ def main(target, label='', ax=None):
         ax.set_xlabel(r'$T_{\rm eff}$ (K)')
         ax.set_ylabel(r'$^{12}$C/$^{13}$C')
         
+    return (Teff_quantiles, carbon_isotope_quantiles)
+        
 
 spirou_sample = {'880': (3720, 4.72, 0.21),
                  '15A': (3603, 4.86, -0.30),
-                '411': (3563, 4.84, 0.12),
+                # '411': (3563, 4.84, 0.12), # TODO: double check this target
                 # '752A': (3558, 4.76, 0.10),
                 # '725B': (3345, 4.96, -0.30),
                 '15B': (3218, 5.07, -0.30),
                 '905': (2930, 5.04, 0.23),
 }
+
+
 targets = ['gl'+t for t in spirou_sample.keys()]
 
 fig, ax = plt.subplots(1,1, figsize=(5,4), tight_layout=True)
 
+Teff_list, C_ratio_list = [], []
 for target in targets:
-    main(target, ax=ax, label=target)
+    Teff_t, C_ratio_t = main(target, ax=ax, label=target)
+    Teff_list.append(Teff_t)
+    C_ratio_list.append(C_ratio_t)
+    
+    # check for binary companion, if so, plot a line connecting the two points
+    if target.endswith('B'):
+        target_A = target[:-1]+'A'
+        idx_A = targets.index(target_A)
+        idx_B = targets.index(target)
+        
+        ax.plot([Teff_list[idx_A][1], Teff_list[idx_B][1]],
+                [C_ratio_list[idx_A][1], C_ratio_list[idx_B][1]], 'k--')
+        
+        
+    
 
 solar = [89.0, 3.0] # review this value...
 ax.axhspan(solar[0]-solar[1], solar[0]+solar[1], color='orange', alpha=0.3, label='Solar',lw=0)
 
 ism = [69.0, 15.0]
 ax.axhspan(ism[0]-ism[1], ism[0]+ism[1], color='green', alpha=0.2, label='ISM',lw=0)
+
+ylim_min = 30.0
+ylim_max = 160.0
+
+ax.set_ylim(ylim_min, ylim_max)
 
 ax.legend()
 ax.set_xlabel(r'$T_{\rm eff}$ (K)')

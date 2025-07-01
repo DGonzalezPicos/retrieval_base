d0e65e18859e9c037779353c1a86ebd51535ba8b
DGonzalezPicos <dariogonzalezpicos@gmail.com>
2025-07-01 10:28:04 +0200

    retrieval G2G3_1 for TWA27A with 800 live points

--- TWA27A/config_jwst.py
+++ TWA27A/config_jwst.py
@@ -12,8 +12,8 @@ lbl = 10
 # run = f'lbl{lbl}_G2G3_8'
 # run = f'lbl{lbl}_G1_2_freechem'
 # grating = 'g235h+g395h'
-gratings = ['g140h', 'g235h', 'g395h']
-# gratings = ['g235h', 'g395h']
+# gratings = ['g140h', 'g235h', 'g395h']
+gratings = ['g235h', 'g395h']
 # gratings = ['g140h', 'g235h', 'g395h']
 # gratings = ['g140h']
 grating_suffix = ''.join([str(g[:2]).upper() for g in gratings]) # e.g. G1G2
@@ -23,7 +23,7 @@ chem_mode = 'fastchem'
 cov_mode = 'newGP' # NEW 2025-02-27: use new GP mode, keep OLDCovariance for compatibility
 cov_mode_label = f'_{cov_mode}' if cov_mode != 'None' else ''
 
-index = 1
+index = 2
 run = f'freeslab_lbl{lbl}_{grating_suffix}_{index}'
 # run = 'test_g395h'
 prefix = f'./retrieval_outputs/{run}/test_'
